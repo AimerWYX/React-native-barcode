17a9a7fa7fb6ae24265139bf70451f46e8143861
Aimer <w08260826w@gmail.com>
2023-10-17 09:45:32 +0800

    fix redirect

--- MyBarcode/app/navigation/index.tsx
+++ MyBarcode/app/navigation/index.tsx
@@ -9,7 +9,7 @@ const Stack = createNativeStackNavigator();
 
 const Navigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen
